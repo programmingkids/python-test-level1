70277d044426a832972a225534308f0ac5fd29ba
EC2 Default User <ec2-user@ip-172-31-11-152.ap-northeast-1.compute.internal>
2020-07-20 03:11:39 +0000

    added test03

---
+++ test03/test01.py
@@ -0,0 +1,14 @@
+# 実行結果
+# I am Alice
+
+
+name = 
+
+if name == "Micky" :
+    print("I am Mikcy")
+elif name == "Minny" :
+    print("I am Minny")
+elif name == "Alice" :
+    print("I am Alice")
+else :
+    print("Who am I?")
---
+++ test03/test05.py
@@ -0,0 +1,9 @@
+# 実行結果
+# 進みます
+
+signal = 
+
+if signal == "赤" :
+    print("止まります")
+else :
+    print("進みます")
